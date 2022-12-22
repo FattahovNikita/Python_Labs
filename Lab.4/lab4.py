0d31d72e644db641c8d9512b24ac6d39c6604a8b
Nickita <fattahovnikita@mail.ru>
2022-12-22 20:02:25 +0500

    add parser data

--- Lab.4/lab4.py
+++ Lab.4/lab4.py
@@ -8,3 +8,16 @@ num_list = [1, 2, 3, 4, 5]
 print(num_list)
 print(list(map(lambda x: x * 2, num_list)))
 print(reduce(lambda x, y: x + y, num_list))
+
+
+url = "https://perm.kinoafisha.info/cinema/"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "lxml")
+all_cinemas_names = soup.find_all(class_="cinemaList_name")
+cinemas = []
+for item in all_cinemas_names:
+    item_name = item.text
+    cinemas.append(item_name)
+print(cinemas)
+print(list(map(lambda x: "Cinema " + x,cinemas)))
+print(reduce(lambda x, y: x +" " + y, cinemas))
